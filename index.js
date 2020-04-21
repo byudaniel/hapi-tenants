5d61fc75a1ec919063ef1ddb564edc9bcd2565d1
Daniel O'Neill <byudaniel@gmail.com>
2020-04-21 12:53:26 -0400

    Expose enter, setTenantId

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const localStore = require('./localStore')
 module.exports = {
   plugin: require('./hapiTenants'),
   getTenantId: () => localStore.getTenantId(),
+  setTenantId: (tenantId) => localStore.setTenantId(tenantId),
+  enter: () => localStore.enter(),
 }
